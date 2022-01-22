bb616f302ae8be32f7d1b838a70665a1e257448d
mccurdyr <ryan.mccurdy96@gmail.com>
2022-01-22 15:30:36 -0500

    Here's hoping I did this one right!

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,19 @@
 package virtual_pet;
 
 public class VirtualPetApplication {
+    //Creates instances of Chimp
+
 
     public static void main(String[] args) {
+        //New instance of main class
+        Game myChimpGame = new Game ();
+        myChimpGame.play();
+
+
         //Interact with a VirtualPet object in this method
     }
+
+
+
 
 }
